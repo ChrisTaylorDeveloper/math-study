5a1e465966a669b8aa18f7877d3b6e5deed9b6af
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-10-07 10:11:07 +0200

    Remove many sub-sections from Overleaf example.

---
+++ build-pdf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+latex math-study.tex && \
+pdflatex -halt-on-error math-study.tex && \
+rm -f public-html/math-study.pdf && \
+mv math-study.pdf public-html/math-study.pdf
--- create-pdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-latex math-study.tex
-pdflatex -halt-on-error math-study.tex
-rm -f public-html/math-study.pdf
-mv math-study.pdf public-html/math-study.pdf
